3c80ce6473e8733fc7f130eb1fe4deb414e7894e
Arslan Javaid <aja118379@gmail.com>
2024-10-22 15:01:47 +0500

    Connected metamask using ethers.js library

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -1,26 +1,35 @@
 import { useState } from 'react';
+import { ethers } from 'ethers'
 
 function ConnectWallet() {
 
     const [ walletAddress, setWalletAddress ] = useState("")
 
-  const requestAccount = async () => {
-    if (window.ethereum) {
-
-        try {
-            const accounts = await window.ethereum.request({
-                method: "eth_requestAccounts",
-            })
-            setWalletAddress(accounts[0])
-            console.log(accounts)
-        } catch (err) {
-            console.log(err)
+    const requestAccount = async () => {
+        if (window.ethereum) {
+          try {
+            // Request account access
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+    
+            // Create an ethers provider (updated for ethers v6)
+            const provider = new ethers.BrowserProvider(window.ethereum);
+    
+            // Get the signer (the connected wallet account)
+            const signer = await provider.getSigner();
+    
+            // Get the connected wallet address
+            const address = await signer.getAddress();
+    
+            console.log('Connected wallet address:', address);
+    
+            setWalletAddress(address);
+          } catch (err) {
+            console.log(err);
+          }
+        } else {
+          alert('MetaMask not installed');
         }
-
-    } else {
-      alert("MetaMask not installed");
-    }
-  };
+      };
 
   return (
     <div>
